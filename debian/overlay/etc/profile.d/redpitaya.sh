a2f582da5c80718022f1c04beeb2cd8d3a35c741
n.danilyuk <n.danilyuk@integrasources.com>
2024-02-08 10:18:46 +0700

    Added path to python files

--- debian/overlay/etc/profile.d/redpitaya.sh
+++ debian/overlay/etc/profile.d/redpitaya.sh
@@ -1,5 +1,6 @@
 export PATH_REDPITAYA=/opt/redpitaya
 export PATH=$PATH:$PATH_REDPITAYA/sbin:$PATH_REDPITAYA/bin
+export PYTHONPATH=/opt/redpitaya/lib/python/:$PYTHONPATH
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PATH_REDPITAYA/lib
 
 if [[ -f $PATH_REDPITAYA/bin/production_testing_script.sh ]]
